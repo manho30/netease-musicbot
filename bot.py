5b6509dcada7d730963a19347e9dc447f6386eff
manho30 <manhochan30@gmail.com>
2022-05-21 18:53:44 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telepot
+from telepot.loop import MessageLoop
+from searchMusic import searchMusic as search
+from parseMusicList import parseMusicList as parse
+
+bot = telepot.Bot('5187758392:AAGb0ZAzUHdyjKCfxky4GxMwKT0ud5o3EqQ')
+
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    if chat_type == 'private':
+        if msg['text'] == '/start':
+            bot.sendChatAction(chat_id, 'typing')
+            bot.sendMessage(chat_id, 'Hi! I am a bot can send you music from Netease Cloud Music. ' +
+                                      'Simply send me a music name and I will send you the music.')
+                                      
+        else:
+            # the rest of message will be searching for the music handle by searchMusic.py
+            bot.sendChatAction(chat_id, 'typing')
+            searching = bot.sendMessage(chat_id, 'Searching music for ' + msg['text'])
+            
+            result = parse(search(msg['text']), 0, 10)
+            
+            bot.editMessageText(telepot.message_identifier(searching), result, parse_mode='Markdown')
+            
+MessageLoop(bot, handle).run_forever()
---
+++ searchMusic.py
@@ -0,0 +1,22 @@
+'''
+function for search music
+@param: {str} name
+@return: {object} musicInfo
+'''
+
+author = 'manho'
+
+import requests
+
+def searchMusic(name):
+    url = 'https://netease.herokuapp.com/search?keywords={}'.format(name)
+    res = requests.get(url)
+    print (res.json())
+    return res.json()
+    
+    
+# for debug only!
+'''
+if __name__ == '__main__':
+    searchMusic('yihuik')
+'''
